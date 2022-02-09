ef0b06ce170a1adb8590ba051bd2eedf0fe73368
Ruilin Yang <ruilin.yang@protonmail.com>
2022-02-09 22:59:36 +0100

    need to refactor promise chaining

--- buechehalle_crawler/index.js
+++ buechehalle_crawler/index.js
@@ -1,52 +1,124 @@
-/* 
-steps: 
-1. [done] from startURL get two kinds of things: 
-    * [done] total number of pages and url pattern of each page 
-    * [done] urls of each game title
-2. [done] from each pageURL, get a list of gameURL
-3. from each gameURL, get whether the game is available somewhere, 
-write result
-4. (optional) for each location that has an available game, 
-query google map api to find the ones that are nearest to me
-*/
+/*
+ * steps:
+ * 1. [done] from startURL get two kinds of things:
+ *    - [done] total number of pages and url pattern of each page
+ *    - [done] urls of each game title
+ * 2. [done] from each pageURL, get a list of gameURL
+ * 3. from each gameURL, get whether the game is available somewhere,
+ * write result
+ * 4. (optional) for each location that has an available game,
+ * query google map api to find the ones that are nearest to me
+ */
 
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
+const { format } = require("@fast-csv/format");
 const selector = require("./selector");
 
 const CONSOLE = "[Nintendo Switch]";
 const baseURL = "https://www.buecherhallen.de/";
 const pageURL = "katalog-suchergebnisse.html";
 const gameURLs = [];
+const gameAvailInfo = [];
 let totalPage;
 
-// ------------ get urls of all games ------------
 axios
   .get(`${baseURL}${pageURL}`, {
     params: {
       suchbegriff: CONSOLE,
     },
   })
+  // 1. init: get total page, and record game urls on first page
   .then((response) => {
     const $ = getParsedPage(response);
     _recordGameURLOnPage($);
     totalPage = _getTotalPages($);
+    return totalPage;
   })
+  // 2. record game urls on the rest pages
   .then(() => {
-    console.log("skip recording all game url for now..");
-    // recordRestGameURL();
+    recordRestGameURL();
+    // console.log(`${gameURLs.length} games recorded`);
   })
+  // // 3. record available game info
+  // .then(() => {
+  //   checkAllGames();
+  // })
   .catch((error) => {
     console.error(error);
   });
 
 // ------------ check availability of games ------------
-const tempGame =
-  "https://www.buecherhallen.de/suchergebnis-detail/medium/T019782113.html";
 
-axios.get(`${baseURL}${gameURLs[0]}`).then((response) => {
-  const $ = getParsedPage(response);
-});
+const checkAllGames = () => {
+  // --- attempt to user axios.all ---
+  // const gameAxiosReqs = gameURLs.map((gameURL) => {
+  //   return axios.get(`${baseURL}${gameURL}`);
+  // });
+
+  // axios.all(gameAxiosReqs).then((responses) => {
+  //   responses.forEach((resp) => {
+  //     const $ = getParsedPage(resp);
+  //     if (_anyCopyAvailable($)) {
+  //       _recordGameAvailInfo($);
+
+  //     }
+  //   });
+  // });
+
+  gameURLs.forEach((gameURL) => {
+    _checkOneGame(gameURL);
+  });
+};
+
+const _checkOneGame = (gameURL) => {
+  axios.get(`${baseURL}${gameURL}`).then((response) => {
+    const $ = getParsedPage(response);
+    const availLibraries = $(selector["availLibraries"]);
+    if (availLibraries.length !== 0) {
+      availLibraries.each((i, ele) => {
+        _writeARow($, ele, `${baseURL}${gameURL}`);
+      });
+    }
+  });
+};
+const _writeARow = ($, ele, link) => {
+  // for now just print to console
+  const title = $(selector["title"]).text();
+  const url = link;
+  const library = $(ele).find(selector["library"]).text();
+  const availableCopy = $(ele).find(selector["availableCopy"]).text();
+  const totalCopy = __computeTotalCopyNr($);
+  console.log({
+    title: title,
+    url: url,
+    library: library,
+    availableCopy: availableCopy,
+    totalCopy: totalCopy,
+  });
+};
+
+const _recordGameAvailInfo = ($) => {
+  $(selector["availLibraries"]).each((i, ele) => {});
+};
+
+const _anyCopyAvailable = ($) => {
+  if ($(selector["availLibraries"]).length !== 0) {
+    return true;
+  }
+  return false;
+};
+
+const __computeTotalCopyNr = ($) => {
+  let count = 0;
+  const pattern = /\d+$/;
+  $(selector["availableCopy"]).each((i, ele) => {
+    count += parseInt($(ele).text().match(pattern)[0]);
+  });
+  return count;
+};
+
 // ------------ helper functions ------------
 /*
  * For a response obj from an axios call,
@@ -58,7 +130,8 @@ const getParsedPage = (axiosRes) => {
 };
 
 /*
- * For a cheerio obj, recode all `CONSOLE` game's URL.
+ * For a cheerio obj (of a page of games), recode a game's URL if the
+ * game's title contains `CONSOLE`.
  */
 const _recordGameURLOnPage = ($) => {
   $(selector["gameTitles"]).each((i, ele) => {
--- buechehalle_crawler/selector.js
+++ buechehalle_crawler/selector.js
@@ -3,16 +3,20 @@
  * gameTitles: get all game titles on a page
  * totalPageInfo: get number of total pages
  * ----- one game page -----
- * nrLibraries: get number of library that ever have this game
- * nrCopies: get number of copies each library ever have
+ * totalLibraries: get number of library that ever have this game
+ * totalCopies: get number of copies each library ever have
  * availLibraries: get libraries that this game is available
  * availCopies: get number of copies this game is available
  */
 const selector = {
   gameTitles: "ul.search-results-list li div.search-results-text h2 a",
   totalPageInfo: "div.pagination.block p.pagination-total",
-  nrLibraries:
+  totalLibraries:
     "div.accordion-wrapper ul.accordion-availability li.accordion-item",
+  availLibraries: "li.record-available",
+  title: "h1.medium-detail-title",
+  library: "span.medium-availability-item-title-location",
+  availableCopy: "span.medium-availability-item-title-count",
 };
 
 module.exports = selector;

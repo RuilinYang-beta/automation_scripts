50fdfcf771481b64b70728e8fdc08695b3c6d7cd
Ruilin Yang <ruilin.yang@protonmail.com>
2022-02-09 16:39:50 +0100

    get url of all games

--- buechehalle_crawler/index.js
+++ buechehalle_crawler/index.js
@@ -1,9 +1,9 @@
 /* 
 steps: 
-1. from startURL get two kinds of things: 
+1. [done] from startURL get two kinds of things: 
     * [done] total number of pages and url pattern of each page 
     * [done] urls of each game title
-2. from each pageURL, get a list of gameURL
+2. [done] from each pageURL, get a list of gameURL
 3. from each gameURL, get whether the game is available somewhere, 
 write result
 4. (optional) for each location that has an available game, 
@@ -14,89 +14,94 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const selector = require("./selector");
 
+const CONSOLE = "[Nintendo Switch]";
 const baseURL = "https://www.buecherhallen.de/";
 const pageURL = "katalog-suchergebnisse.html";
 const gameURLs = [];
 let totalPage;
 
+// ------------ get urls of all games ------------
 axios
   .get(`${baseURL}${pageURL}`, {
     params: {
-      suchbegriff: "[Nintendo Switch]",
+      suchbegriff: CONSOLE,
     },
   })
   .then((response) => {
-    // const startPage = response["data"];
-    // const $ = cheerio.load(startPage, null, false);
     const $ = getParsedPage(response);
     _recordGameURLOnPage($);
     totalPage = _getTotalPages($);
   })
   .then(() => {
-    recordRestGameURL();
+    console.log("skip recording all game url for now..");
+    // recordRestGameURL();
   })
   .catch((error) => {
     console.error(error);
   });
 
+// ------------ check availability of games ------------
+const tempGame =
+  "https://www.buecherhallen.de/suchergebnis-detail/medium/T019782113.html";
+
+axios.get(`${baseURL}${gameURLs[0]}`).then((response) => {
+  const $ = getParsedPage(response);
+});
+// ------------ helper functions ------------
+/*
+ * For a response obj from an axios call,
+ * return a parsed cheerio obj.
+ */
 const getParsedPage = (axiosRes) => {
-  console.log(Object.keys(axiosRes));
   const rawPage = axiosRes["data"];
   return cheerio.load(rawPage, null, false);
 };
 
-const recordRestGameURL = () => {
-  // construct many axios.get Promise and fire them
-  // extract the result and record them somewhere
-
-  // TODO: what is ...?
-  const restPageNr = [...Array(totalPage).keys()]
-    .map((e) => e + 1)
-    .filter((e) => e >= 2);
-  //   console.log(restPageNr);
-
-  // ----- test on 1 page -----
-  //   const e = restPageNr[0];
-  //   axios
-  //     .get(`${baseURL}${pageURL}`, {
-  //       params: {
-  //         suchbegriff: "[Nintendo Switch]",
-  //         "seite-m37": e,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       const $ = getParsedPage(response);
-  //       _recordGameURLOnPage($);
-  //     });
-
-  const restAxiosReqs = restPageNr.map((e) => {
-    axios.get(`${baseURL}${pageURL}`, {
-      params: {
-        suchbegriff: "[Nintendo Switch]",
-        "seite-m37": e,
-      },
-    });
-  });
-
-  //   axios.all(restAxiosReqs).then((responses) => {
-  //     console.log(responses.length);
-  //     responses.forEach((resp) => {
-  //       _recordGameURLOnPage(getParsedPage(resp));
-  //     });
-  //   });
-};
-
+/*
+ * For a cheerio obj, recode all `CONSOLE` game's URL.
+ */
 const _recordGameURLOnPage = ($) => {
-  $(selector["gameTitle"]).each((i, ele) => {
-    if ($(ele).text().includes("[Nintendo Switch]")) {
-      console.log($(ele).attr("href"));
+  $(selector["gameTitles"]).each((i, ele) => {
+    if ($(ele).text().includes(CONSOLE)) {
       gameURLs.push($(ele).attr("href"));
     }
   });
 };
 
+/*
+ * From the cheerio obj of the first page,
+ * get the number of total pages.
+ */
 const _getTotalPages = ($) => {
   const totalPageInfo = $(selector["totalPageInfo"]).text();
   const pattern = /\d+$/;
   return parseInt(totalPageInfo.match(pattern)[0]);
+};
+
+/*
+ * Construct many axios.get Promise and fire them,
+ * extract the game URL of the many responses and record them.
+ */
+const recordRestGameURL = () => {
+  // TODO: what is ...?
+  const restPageNrs = [...Array(totalPage).keys()]
+    .map((e) => e + 1)
+    .filter((e) => e >= 2);
+
+  // get an array of axios get promise
+  const restAxiosReqs = restPageNrs.map((e) => {
+    return axios.get(`${baseURL}${pageURL}`, {
+      params: {
+        suchbegriff: CONSOLE,
+        "seite-m37": e,
+      },
+    });
+  });
+
+  axios.all(restAxiosReqs).then((responses) => {
+    responses.forEach((resp) => {
+      _recordGameURLOnPage(getParsedPage(resp));
+    });
+    console.log(`${gameURLs.length} games recorded`);
+  });
 };
